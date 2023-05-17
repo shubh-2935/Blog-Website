a3374dea6fa91f7e275e3675bd2e5fa080c70e32
Shubham Chaudhari <shubhamvc35@gmail.com>
2023-05-17 19:18:54 +0530

    views path change

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const _ = require("lodash");
 const mongoose = require("mongoose");
 const  ObjectID = require('mongodb').ObjectId;
 require("dotenv").config();
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public")); 
